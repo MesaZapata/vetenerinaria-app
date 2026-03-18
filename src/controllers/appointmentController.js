152996c2d89dd9a08d49b2c74cf781ee4c8c053f
MesaZapata <mesazapatasamuel@gmail.com>
2026-03-18 11:37:14 -0500

    sprint 1 HU 2

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -1,25 +1,18 @@
-const db = require('../config/db');
+const Appointment = require('../models/Appointment');
+const Owner = require('../models/Owner');
+const Pet = require('../models/Pet');
 
 exports.getAllAppointments = (req, res) => {
-    const sql = `SELECT a.*, p.name as pet_name, o.name as owner_name 
-                 FROM appointments a
-                 JOIN pets p ON a.pet_id = p.id
-                 JOIN owners o ON p.owner_id = o.id
-                 ORDER BY a.appointment_date DESC`;
-    
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-            return res.status(500).send(err.message);
-        }
-        res.render('index', { title: 'Panel de Citas', appointments: rows });
+    Appointment.findAllWithDetails((err, appointments) => {
+        if (err) return res.status(500).send(err.message);
+        res.render('index', { title: 'Panel de Citas', appointments });
     });
 };
 
 exports.getCreateForm = (req, res) => {
-    // Get owners and pets for dropdowns
-    db.all("SELECT * FROM owners", [], (err, owners) => {
+    Owner.findAll((err, owners) => {
         if (err) return res.status(500).send(err.message);
-        db.all("SELECT * FROM pets", [], (err, pets) => {
+        Pet.findAll((err, pets) => {
             if (err) return res.status(500).send(err.message);
             res.render('create', { title: 'Agendar Nueva Cita', owners, pets });
         });
@@ -28,22 +21,15 @@ exports.getCreateForm = (req, res) => {
 
 exports.createAppointment = (req, res) => {
     const { pet_id, service, appointment_date } = req.body;
-    const sql = "INSERT INTO appointments (pet_id, service, appointment_date) VALUES (?, ?, ?)";
-    
-    db.run(sql, [pet_id, service, appointment_date], function (err) {
-        if (err) {
-            return res.status(500).send(err.message);
-        }
+    Appointment.create({ pet_id, service, appointment_date }, function (err) {
+        if (err) return res.status(500).send(err.message);
         res.redirect('/');
     });
 };
 
 exports.deleteAppointment = (req, res) => {
-    const id = req.params.id;
-    db.run("DELETE FROM appointments WHERE id = ?", id, function (err) {
-        if (err) {
-            return res.status(500).send(err.message);
-        }
+    Appointment.deleteById(req.params.id, function (err) {
+        if (err) return res.status(500).send(err.message);
         res.redirect('/');
     });
 };
